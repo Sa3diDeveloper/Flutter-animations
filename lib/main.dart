d60a7d655becc06b6efa55e68152b4d41882a364
sa3di Develoepr <sa3dideveloper@gmail.com>
2023-05-23 07:50:54 -0700

    animations btn nav bar 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:animations/animations/buttom_navigation_bar/btn_nav_bar.2dart';
 import 'package:animations/animations/buttom_navigation_bar/btn_nav_bar1.dart';
+import 'package:animations/animations/buttom_navigation_bar/btn_nav_bar3.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BtnNavBar(),
+      home: HomePage111(),
     );
   }
 }
